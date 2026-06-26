36e7fa5bcdad854b7e107cfe08d4f7bad447beb8
iamsamhitaacharya <iamsamhitaacharya@gmail.com>
2026-06-26 21:39:23 +0545

    add Joi schema for listing validation

---
+++ schema.js
@@ -0,0 +1,16 @@
+const Joi=require("joi");
+
+const listingSchema = Joi.object({
+    listing : Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required().max(500),
+        location: Joi.string().required(),
+        country: Joi.string().required().min(0),
+        price: Joi.number().required(),
+        image: Joi.object({
+            url:Joi.string().allow("",null)
+        }),
+    }).required()
+});
+
+module.exports = { listingSchema };

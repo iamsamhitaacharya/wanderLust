ad81f46de1aed62b19ac3c6f54fbf4534d291441
iamsamhitaacharya <iamsamhitaacharya@gmail.com>
2026-07-03 19:45:36 +0545

    refactor: simplify review handling by delegating to reviews route

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
-const wrapAsync = require("./utils/wrapAsync.js");
-const {listingSchema, reviewSchema} = require("./schema.js");
-const Review = require("./models/review.js"); 
 
 const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 
 app.set("view engine", "ejs");
@@ -42,44 +39,10 @@ app.get("/", (req, res) => {
 
 
 
-//---------review validating function----------
-const validateReview = (req, res, next ) => {
-    let {error} = reviewSchema.validate(req.body);
-    if(error){
-        let errMsg=error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    }else{
-        next();
-    }
-};
 
 
 app.use("/listings", listings);
-
-//------------------Reviews-------------
-//-------------Post Route----------------
-app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req,res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    listing.reviews.push(newReview);
-    await newReview.save();
-    await listing.save();
-    console.log("new review saved");
-    res.redirect(`/listings/${listing._id}`);
-})
-);
-
-
-
-//------------Delete Route-----------
-app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async(req,res)=> {
-    let { id, reviewId } = req.params;
-    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/listings/${id}`);
-})
-);
-
+app.use("/listings/:id/reviews", reviews);
 
 
 //--------handling error for random path in the same domain----------

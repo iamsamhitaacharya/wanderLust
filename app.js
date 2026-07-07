82e077c581e11a99c0c437fc7a9410778ec14a0d
iamsamhitaacharya <iamsamhitaacharya@gmail.com>
2026-07-07 16:21:22 +0545

    feat: implement session management and flash messaging for notifications

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
@@ -17,6 +19,34 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+const sessionOptions = {
+    secret: "mysupersecret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60* 1000,
+        maxAge: 7*24*60*60*1000,
+        httpOnly: true,
+    },
+};
+
+
+app.get("/", (req, res) => {
+    res.send("this is your root");
+});
+
+
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+
+app.use((req,res,next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    // console.log(res.locals.success);
+    next();
+});
 
 main().then(()=> {
     console.log("connected to Db");
@@ -27,15 +57,6 @@ main().then(()=> {
 async function main(){
     await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 }
-
-
-
-app.get("/", (req, res) => {
-    res.send("this is your root");
-});
-
-
-
 
 
 

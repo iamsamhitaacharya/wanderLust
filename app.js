f89526bd8188ce97c0bd91f36f68204ffbca53ca
iamsamhitaacharya <iamsamhitaacharya@gmail.com>
2026-07-09 11:12:29 +0545

    feat: integrate passport for authentication and connect to MongoDB

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter  = require("./routes/user.js");
 
 
 app.set("view engine", "ejs");
@@ -31,23 +35,6 @@ const sessionOptions = {
 };
 
 
-app.get("/", (req, res) => {
-    res.send("this is your root");
-});
-
-
-
-app.use(session(sessionOptions));
-app.use(flash());
-
-
-app.use((req,res,next) => {
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    // console.log(res.locals.success);
-    next();
-});
-
 main().then(()=> {
     console.log("connected to Db");
 })
@@ -59,11 +46,44 @@ async function main(){
 }
 
 
+app.get("/", (req, res) => {
+    res.send("this is your root");
+});
 
 
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use((req,res,next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    // console.log(res.locals.success);
+    next();
+});
+
+
+// app.get("/demouser", async(req,res) => {
+//     let fakeUser = new User({
+//         email:"john@gmail.com",
+//         username: "john",
+//     });
+//     let registeredUser = await User.register(fakeUser,"helloworld");
+//     res.send(registeredUser);
+// });
+
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 
 //--------handling error for random path in the same domain----------

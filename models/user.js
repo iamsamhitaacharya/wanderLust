f75c431dde9ad472fce615025ae4e9513220ab0e
iamsamhitaacharya <iamsamhitaacharya@gmail.com>
2026-07-09 11:13:21 +0545

    fix: correct import statement and schema plugin usage in user model

--- models/user.js
+++ models/user.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require(passport-local-mongooose);
+const passportLocalMongoose = require("passport-local-mongoose").default;
 
 const userSchema = new Schema({
     email: {
         type: String,
-        required: True,
+        required: true,
     }
 });
 
-User.plugin(passportLocalMongoose);
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);

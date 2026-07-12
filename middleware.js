9fe4a24052248682e2f4d3adb3fd5d95a33d285a
iamsamhitaacharya <iamsamhitaacharya@gmail.com>
2026-07-12 17:30:39 +0545

    feat: add isAuthor middleware for review ownership validation

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 const ExpressError = require("./utils/ExpressError.js");
 const {listingSchema, reviewSchema} = require("./schema.js");
 const Listing = require("./models/listing.js");
+const Review = require("./models/review.js");
 
 
 
@@ -25,6 +26,16 @@ module.exports.isOwner = async (req, res, next) => {
     let listing = await Listing.findById(id);
     if( !listing.owner._id.equals(res.locals.currUser._id)){
         req.flash("error", "You are not the owner of this listing!");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+}
+
+module.exports.isAuthor = async (req, res, next) => {
+    let {id, reviewId}=req.params;
+    let review = await Review.findById(reviewId);
+    if( !review.author._id.equals(res.locals.currUser._id)){
+        req.flash("error", "You are not the author of this review!");
         return res.redirect(`/listings/${id}`);
     }
     next();

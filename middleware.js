569bfe56b4b95f3fa39bf45b1e513fb573e35029
iamsamhitaacharya <iamsamhitaacharya@gmail.com>
2026-07-12 16:38:01 +0545

    feat: add isLoggedIn middleware and isOwner middleware for enhanced user authorization

--- middleware.js
+++ middleware.js
@@ -1,5 +1,10 @@
-module.exports.isLoggedIn = (req,res,next) => {
-    
+const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema, reviewSchema} = require("./schema.js");
+const Listing = require("./models/listing.js");
+
+
+
+module.exports.isLoggedIn = (req,res,next) => { 
     if(!req.isAuthenticated()){
         req.session.redirectUrl = req.originalUrl;
         req.flash("error", "You must be logged in!");
@@ -14,3 +19,35 @@ module.exports.saveRedirectUrl = (req,res,next)=> {
     }
     next();
 }
+
+module.exports.isOwner = async (req, res, next) => {
+    let {id}=req.params;
+    let listing = await Listing.findById(id);
+    if( !listing.owner._id.equals(res.locals.currUser._id)){
+        req.flash("error", "You are not the owner of this listing!");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+}
+
+//-----------------listing validation function--------
+module.exports.validateListing = (req, res, next ) => {
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg=error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }else{
+        next();
+    }
+};
+
+//---------review validating function----------
+module.exports.validateReview = (req, res, next ) => {
+    let {error} = reviewSchema.validate(req.body);
+    if(error){
+        let errMsg=error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }else{
+        next();
+    }
+};

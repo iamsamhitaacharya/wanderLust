e05da5afc6207d5b3d13fe229d719944263e8201
iamsamhitaacharya <iamsamhitaacharya@gmail.com>
2026-07-13 21:33:43 +0545

    feat: refactor review routes to use controller methods for improved maintainability

--- routes/review.js
+++ routes/review.js
@@ -5,33 +5,13 @@ const Listing = require("../models/listing.js");
 const Review = require("../models/review.js"); 
 const {validateReview, isLoggedIn, isAuthor} = require("../middleware.js");
 
+const reviewController = require("../controllers/review.js");
 
-//------------------Reviews-------------
 //-------------Post Route----------------
-router.post("/",isLoggedIn,  validateReview, wrapAsync(async(req,res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    newReview.author = req.user._id;
-    listing.reviews.push(newReview);
-    await newReview.save();
-    await listing.save();
-    req.flash("success", "Review Created!");
-    console.log("new review saved");
-    res.redirect(`/listings/${listing._id}`);
-})
-);
-
-
+router.post("/",isLoggedIn,  validateReview, wrapAsync(reviewController.createReview));
 
 //------------Delete Route-----------
-router.delete("/:reviewId", isLoggedIn, isAuthor,  wrapAsync(async(req,res)=> {
-    let { id, reviewId } = req.params;
-    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success", "Review deleted!");
-    res.redirect(`/listings/${id}`);
-})
-);
+router.delete("/:reviewId", isLoggedIn, isAuthor,  wrapAsync(reviewController.deleteReview));
 
 module.exports = router;
 

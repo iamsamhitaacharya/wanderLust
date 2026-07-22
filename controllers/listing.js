bc7f19e76b221a61798bde3c59ae01b308486bcc
iamsamhitaacharya <iamsamhitaacharya@gmail.com>
2026-07-22 12:00:09 +0530

    feat: enhance edit and update listing functionality with image handling

--- controllers/listing.js
+++ controllers/listing.js
@@ -47,12 +47,20 @@ module.exports.editListing = async (req,res,next) => {
         req.flash("error", "Listing you requested for does not exist!");
         return res.redirect("/listings");
     }
-      res.render("listings/edit.ejs", {listing});
+    let originalImage = listing.image.url;
+    originalImage= originalImage.replace("/upload", "/upload/w_250");
+    res.render("listings/edit.ejs", {listing, originalImage});
  };
 
- module.exports.renderUpdateForm = async (req,res,next) => {
+ module.exports.updateListing = async (req,res,next) => {
     let {id}=req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    let listing = await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    if(typeof req.file !=="undefined"){
+    let url = req.file.path;
+    let filename = req.file.filename;
+    listing.image = { url, filename};
+    await listing.save();
+    }
     req.flash("success", "Listing updated!");
     res.redirect(`/listings/${id}`);
  };
